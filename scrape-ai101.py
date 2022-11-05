5dc1c8cc91ce419141cf8faf3da51c5c99f8f1ba
malinoal <a.malinowski@campus.tu-berlin.de>
2022-11-05 12:38:11 +0100

    Fix tabs sometimes not waited for

--- scrape-ai101.py
+++ scrape-ai101.py
@@ -1,15 +1,13 @@
 from playwright.sync_api import sync_playwright
 
-def save_curriculum(url: str, filename: str, waitforzero: bool=False):
+def save_curriculum(url: str, filename: str, waitfortab: str):
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
         page.goto(url)
         #class="c-tabs__accordion-tab"
         #c-tabs__accordion-tab
-        if waitforzero:
-            #AI Governance has other tabs that are loaded first and we want to wait until the actual tabs are loaded
-            week0 = page.locator('css=#tab-week-0').wait_for()
+        week0 = page.locator('css=#' + waitfortab).wait_for()
         tabs = page.locator('css=.c-tabs__accordion-tab')
         for i in range(tabs.count()):
             weekitab = tabs.nth(i)
@@ -18,6 +16,6 @@ def save_curriculum(url: str, filename: str, waitforzero: bool=False):
         browser.close()
 
 
-save_curriculum('https://www.agisafetyfundamentals.com/ai-alignment-curriculum', 'alignment101.pdf')
-save_curriculum('https://www.agisafetyfundamentals.com/alignment-201-curriculum', 'alignment201.pdf')
-save_curriculum('https://www.agisafetyfundamentals.com/ai-governance-curriculum', 'aigovernance.pdf', True)
+save_curriculum('https://www.agisafetyfundamentals.com/ai-alignment-curriculum', 'alignment101.pdf', 'tab-week-0')
+save_curriculum('https://www.agisafetyfundamentals.com/alignment-201-curriculum', 'alignment201.pdf', 'tab-blog-post-title-one-7mj4n')
+save_curriculum('https://www.agisafetyfundamentals.com/ai-governance-curriculum', 'aigovernance.pdf', 'tab-week-0')
