122f2ab38ebd03f62512958f15a8ca8c06f203d9
malinoal <a.malinowski@campus.tu-berlin.de>
2022-11-05 12:27:46 +0100

    Generalise to other curricula

--- scrape-ai101.py
+++ scrape-ai101.py
@@ -1,21 +1,23 @@
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto('https://www.agisafetyfundamentals.com/ai-alignment-curriculum')
-    for i in range(9):
-        #id="tab-week-i"
-        weekitab = page.locator("css=#tab-week-" + str(i))
-        weekitab.click()
-        #tabpanel-week-i
-        weekicontent = page.locator("css=#tabpanel-week-" + str(i))
-        weekicontent.click()
-    #tab-further-alignment-resources
-    #tabpanel-further-alignment-resources
-    furthercontenttab = page.locator("css=#tab-further-alignment-resources")
-    furthercontenttab.click()
-    furthercontentcontent = page.locator("css=#tabpanel-further-alignment-resources")
-    furthercontentcontent.click()
-    page.pdf(path=f'alignment101.pdf')
-    browser.close()
+def save_curriculum(url: str, filename: str, waitforzero: bool=False):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        #class="c-tabs__accordion-tab"
+        #c-tabs__accordion-tab
+        if waitforzero:
+            #AI Governance has other tabs that are loaded first and we want to wait until the actual tabs are loaded
+            week0 = page.locator('css=#tab-week-0').wait_for()
+        tabs = page.locator('css=.c-tabs__accordion-tab')
+        for i in range(tabs.count()):
+            weekitab = tabs.nth(i)
+            weekitab.click()
+        page.pdf(path=filename)
+        browser.close()
+
+
+save_curriculum('https://www.agisafetyfundamentals.com/ai-alignment-curriculum', 'alignment101.pdf')
+save_curriculum('https://www.agisafetyfundamentals.com/alignment-201-curriculum', 'alignment201.pdf')
+save_curriculum('https://www.agisafetyfundamentals.com/ai-governance-curriculum', 'aigovernance.pdf', True)
